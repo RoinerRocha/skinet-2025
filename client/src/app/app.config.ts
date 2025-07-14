c9faf4fdbc532afce4b77490ff73e54682778491
Roiner Rocha <rochabozaroiner@gmail.com>
2025-07-14 13:27:12 -0600

    End of Section 11

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,18 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(),
-    {
-      provide: MAT_DIALOG_DEFAULT_OPTIONS,
-      useValue: { autoFocus: 'dialog', restoreFocus: true }
-    }
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
   ]
 };

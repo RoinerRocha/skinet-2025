248d10a6518383bb025280308c744d4e4e7e4085
Roiner Rocha <rochabozaroiner@gmail.com>
2025-08-24 15:25:17 -0600

    Pre continous integration

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51Rs6CPEHGIEO4xCvZjtTXcb4t87h5gOhTvQ5ypk2UD84oDvvArd6arcL6q8VxGzH6bBDtZ0wfHTR8iwuxZX0Nf7e006KP7iRxH'
+    stripePublicKey: 'pk_test_51RziKfQBN4sN3kZTjMGeoW69HZ4uswbFMPYw31l6yyfOz9w9LRB51hTb5XbQZD37JnzK7dL1gGbkopU0sezb2zYe00aXIyXVtQ'
 };
